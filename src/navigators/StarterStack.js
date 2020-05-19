c75adc338ae48ecc1d5288e753631594c08841c2
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-18 21:32:20 -0300

    Add starter stack

--- src/navigators/StarterStack.js
+++ src/navigators/StarterStack.js
@@ -1,7 +1,9 @@
 import { createStackNavigator } from 'react-navigation-stack';
 
 import StarterIntro from '../screens/StarterIntro';
+import StarterName from '../screens/StarterName';
 
 export default createStackNavigator({
-    StarterIntro
+    StarterIntro,
+    StarterName
 });
