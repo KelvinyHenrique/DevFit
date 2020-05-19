ef9cc53e819da420830a5e69443c17af55a5ee31
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-19 10:50:47 -0300

    Bug no change backgroundColor

--- src/screens/StarterDias.js
+++ src/screens/StarterDias.js
@@ -48,16 +48,18 @@ const Page = (props) => {
     //Novo
     const [days, setDays] = useState([]);
 
-    const toggleDay = (d) => {
+    const addDay = (d) => {
         let newDays = [...days];
         if(!newDays.includes(d)) {
             newDays.push(d);
+            console.log("Entrou aqui");
         } else {
             newDays = newDays.filter(i=>i!=d);
         }
         setDays(newDays);
         props.setWorkoutDays(newDays);
         props.navigation.setParams({days:newDays});
+        alert(days);
     }
     
    /*  const toggleDay = (d) => { 
@@ -65,7 +67,7 @@ const Page = (props) => {
         if(!props.workoutDays.includes(d)){
             //Inserir
             newWorkoutDays.push(d);
-            console.log("Ahhhhhhhhhhhhhhh desgraçaaaaa");
+            console.log("Entrou aqui");
         } else {
             //Remover
             newWorkoutDays = newWorkoutDays.filter(i=>i!=d);
@@ -83,25 +85,25 @@ const Page = (props) => {
             <HeaderText>Quais <Text style={{fontWeight:'bold'}}>dias da semana</Text> você pretende treinar?</HeaderText>
 
             <DaysArea>
-                <DefaultButton  bgcolor={props.workoutDays.includes(1)?'#FF0000':false} onPress={()=>toggleDay(1)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(1)} bgcolor={days.includes(1)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Segunda</Text>
                 </DefaultButton>
-                <DefaultButton bgcolor={props.workoutDays.includes(2)?'#FF0000':false}  onPress={()=>toggleDay(2)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(2)} bgcolor={days.includes(2)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Terça</Text>
                 </DefaultButton>
-                <DefaultButton bgcolor={props.workoutDays.includes(3)?'#FF0000':false}  onPress={()=>toggleDay(3)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(3)} bgcolor={days.includes(3)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Quarta</Text>
                 </DefaultButton>
-                <DefaultButton bgcolor={props.workoutDays.includes(4)?'#FF0000':false}  onPress={()=>toggleDay(4)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(4)} bgcolor={days.includes(4)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Quinta</Text>
                 </DefaultButton>
-                <DefaultButton bgcolor={props.workoutDays.includes(5)?'#FF0000':false}  onPress={()=>toggleDay(5)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(5)} bgcolor={days.includes(5)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Sexta</Text>
                 </DefaultButton>
-                <DefaultButton bgcolor={props.workoutDays.includes(6)?'#FF0000':false}  onPress={()=>toggleDay(6)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(6)} bgcolor={days.includes(6)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Sábado</Text>
                 </DefaultButton>
-                <DefaultButton bgcolor={props.workoutDays.includes(0)?'#FF0000':false}  onPress={()=>toggleDay(0)}  width={100} style={{marginBottom:20}} underlayColor="#CCC">
+                <DefaultButton onPress={()=>addDay(0)} bgcolor={days.includes(0)?'#a5e8bc':false} width={100} style={{marginBottom:20}} underlayColor="#CCC">
                     <Text>Domingo</Text>
                 </DefaultButton>
             </DaysArea>
