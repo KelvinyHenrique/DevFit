6812d48d78fa150f9c7f79988c4dc3cafa2f670b
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-20 12:46:22 -0300

    Add screens

--- src/navigators/StarterStack.js
+++ src/navigators/StarterStack.js
@@ -3,10 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import StarterIntro from '../screens/StarterIntro';
 import StarterName from '../screens/StarterName';
 import StarterDias from '../screens/StarterDias';
+import StarterNivel from '../screens/StarterNivel';
+import StarterRecommendations from '../screens/StarterRecommendations';
 
 
 export default createStackNavigator({
     StarterIntro,
     StarterName,
-    StarterDias
+    StarterDias,
+    StarterNivel,
+    StarterRecommendations
 });
