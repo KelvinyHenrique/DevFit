c96994ce2df82741789dec44019dcd9262c54349
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-28 10:19:14 -0300

    Home Header build

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text, Button } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
 import styled from 'styled-components/native';
-import {Text} from 'react-native';
+import { connect } from 'react-redux';
 
 
-const Container = styled.View``;
+const Container = styled.SafeAreaView`
+    flex: 1;
+    alignItems:center;
+    backgroundColor:#FFF;
+    paddingLeft:30px;
+    paddingRight: 30px; 
+    paddingTop:50px;
+
+`;
+
+const Page = (props) => {
 
 
-export default () => {
     return (
         <Container>
-            <Text>Página Home</Text>
+
         </Container>
     );
 }
+
+Page.navigationOptions = ({ navigation }) => {
+
+    const ConfigButtonArea = styled.TouchableHighlight`
+        width:30px;
+        height:30px;
+        justifyContent:center;
+        alignItems: center;
+        `;
+    const ConfigButtonImage = styled.Image`
+        width:25px;
+        height:25px;
+    `;
+
+    const ConfigButton = () => {
+        
+        const btnAction = () => {
+            navigation.navigate('HomeConfig');
+        }
+
+        return (
+            <ConfigButtonArea onPress={btnAction}>
+                <ConfigButtonImage source={require('../assets/config.png')} />
+            </ConfigButtonArea>
+        );
+    }
+
+    return {
+        title: 'Seu progresso diário',
+        headerRight: () => <ConfigButton />,
+        headerRightContainerStyle:{
+            marginRight:10
+        }
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+    }
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
