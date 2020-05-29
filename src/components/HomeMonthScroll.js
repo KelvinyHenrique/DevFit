619dd455dc248254e1f2a8281f6b8d3d52efde46
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-29 09:55:42 -0300

    Finish MonthScroll

--- src/components/HomeMonthScroll.js
+++ src/components/HomeMonthScroll.js
@@ -8,57 +8,68 @@ const MonthScroll = styled.ScrollView`
 `;
 const MonthButton = styled.TouchableHighlight`
     width:${props=>props.width}px;
-    justifyContent:center;
-    alignItems:center;
+    justify-content:center;
+    align-items:center;
 `;
 const MonthItem = styled.View`
-    width: 90%;
+    width:90%;
     height:30px;
-    backgroundColor:#EEE;
-    borderRadius:15px;
-    justifyContent:center;
-    alignItems:center;
-
+    background-color:#EEE;
+    justify-content:center;
+    align-items:center;
+    border-radius:15px;
 `;
 const MonthText = styled.Text``;
 
-let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+let MonthItemSelected = {backgroundColor:'#CCC', width:'100%', height:40};
+
 const screenWidth = Math.round(Dimensions.get('window').width);
 let thirdW = screenWidth / 3;
+let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
 export default (props) => {
-    
-   const MonthsRef = useRef();
-
+    const MonthRef = useRef();
     const [selectedMonth, setSelectedMonth] = useState(props.selectedMonth);
-    
-    const handleScrollEnd = (e) => {
-    let posX = e.nativeEvent.contentOffset.x;
-    let targetMonth = Math.round(posX  / thirdW);
-    setSelectedMonth(targetMonth);
-}
-    
-    useEffect(() => {
-        props.setSelectedMonth(selectedMonth);
-    }, [selectedMonth]);    
 
+    const scrollToMonth = (m) => {
+        let posX = m * thirdW;
+        MonthRef.current.scrollTo({x:posX, y: 0, animated: true});
+    }
+
+    const monthScrollEndAction = (e) => {
+        let posX = e.nativeEvent.contentOffset.x;
+        let targetMonth = Math.round(posX / thirdW);
+        setSelectedMonth(targetMonth);
+    }
+
+    useEffect(()=>{
+        props.setSelectedMonth(selectedMonth);
+    }, [selectedMonth]);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            scrollToMonth(selectedMonth);
+        }, 10);
+    }, [props.selectedMonth]);
+    
     return (
         <MonthScroll
+            ref={MonthRef}
             horizontal={true}
             showsHorizontalScrollIndicator={false}
             decelerationRate="fast"
             snapToInterval={thirdW}
-            contentContainerStyle={{paddingLeft:thirdW, paddingRight:thirdW}} 
-            onMomentumScrollEnd={handleScrollEnd}
+            snapToAlignment="center"
+            contentContainerStyle={{paddingLeft:thirdW, paddingRight:thirdW}}
+            onMomentumScrollEnd={monthScrollEndAction}
         >
-            {months.map((m, k) => (
-                <MonthButton key={k} width={thirdW} >
-                    <MonthItem>
+            {months.map((m,k)=>(
+                <MonthButton width={thirdW} key={k} onPress={()=>setSelectedMonth(k)} underlayColor="transparent">
+                    <MonthItem style={k==(selectedMonth)?MonthItemSelected:{}}>
                         <MonthText>{m}</MonthText>
                     </MonthItem>
-
                 </MonthButton>
             ))}
         </MonthScroll>
     );
-}
+};
