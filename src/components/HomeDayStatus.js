6273e96ec610b7cddfd6c59358612edd3df88bab
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-01 12:32:37 -0300

    Bug in the display of items

--- src/components/HomeDayStatus.js
+++ src/components/HomeDayStatus.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import styled  from 'styled-components/native';
+import DefaultButton from '../components/DefaultButton';
 
 
 const BalloonTriangle = styled.View`
     width:0;
     height:0;
     borderLeftColor:transparent;
-    borderLeftWidth:15;
-    borderBottomWidth:15;
+    borderLeftWidth:15px;
+    borderBottomWidth:15px;
     borderBottomColor:#dbd9d9;
-    borderRightWidth:15;
+    borderRightWidth:15px;
     borderRightColor:transparent;
 
 `;
@@ -21,13 +22,93 @@ const BallonArea = styled.View`
     min-height:100px;
 `;
 
+const BalloonBigText = styled.Text`
+    fontSize:15px;
+`;
+
+const ButtonText = styled.Text`
+    color:#FFF;
+    fontWeight:bold;
+`;
+
+const BalloonText = styled.Text`
+    fontSize:13px;
+`;
 
 export default (props) => {
+
+    let today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+
+    let thisDate = new Date(today.getFullYear(), props.selectedMonth, props.selectedDay);
+
+    let thisYear = thisDate.getFullYear();
+    let thisMonth = thisDate.getMonth() + 1;
+    let thisDay = thisDate.getDate();
+    thisMonth = (thisMonth < 10)?'0'+thisMonth:thisMonth;
+    thisDay = (thisDay < 10)?'0'+thisDay:thisDay;
+    let thisFormated = `${thisYear}-${thisMonth}-${thisDay}`;
+
+    let dayOff = false;
+    let isToday = false;
+    let isFuture = false;
+    let isDone = false;
     
+    if(props.workoutDays.includes(thisDate.getDay())) {
+        dayOff =  true;
+    } else if(thisDate.getTime() > today.getTime()) {
+        isFuture = true;
+    } else {
+        if(props.dailyProgress.includes(thisFormated)) {
+            isDone = true;
+        } else {
+            isDone = false;
+        }
+    }
+
+    if(thisDate.getTime() == today.getTime()) {
+        isToday = true;
+    }
+
     return (
         <>
             <BalloonTriangle></BalloonTriangle>
-            <BallonArea></BallonArea>
+            <BallonArea>
+            {dayOff &&
+                    <BalloonBigText>Dia de descanso!</BalloonBigText>
+                }
+                {isFuture &&
+                    <BalloonBigText>Data no futuro</BalloonBigText>
+                }
+                {!dayOff && !isFuture && isDone &&
+                    <>
+                    <BalloonBigText><Strong>Parabéns</Strong>, você treinou!</BalloonBigText>
+                    <DefaultButton>
+                        <ButtonText>DESMARCAR</ButtonText>
+                    </DefaultButton>
+                    </>
+                }
+                {!dayOff && !isFuture && !isDone && !isToday &&
+                    <>
+                    <BalloonBigText>Fraco! Você falhou neste dia.</BalloonBigText>
+                    <DefaultButton >
+                        <ButtonText>MARCAR COMO FEITO</ButtonText>
+                    </DefaultButton>
+                    </>
+                }
+                {!dayOff && !isFuture && !isDone && isToday &&
+                    <>
+                    <BalloonBigText>HOJE TEM TREINO 🚀</BalloonBigText>
+                    <BalloonText>Você tem ... pra treinar</BalloonText>
+                    <DefaultButton>
+                        <ButtonText>INICIAR TREINO</ButtonText>
+                    </DefaultButton>
+                    </>
+                }
+            </BallonArea>
         </>
     );
 }
