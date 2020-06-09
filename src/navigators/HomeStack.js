dcaf541e21296a6b1737896b8f48586ae5a09acc
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-09 10:41:23 -0300

    Add screen HomeConfig

--- src/navigators/HomeStack.js
+++ src/navigators/HomeStack.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Home from '../screens/Home';  
-//import HomeConfig from '../screens/HomeConfig';
+import HomeConfig from '../screens/HomeConfig';
 
 export default createStackNavigator({
     Home,
-   //HomeConfig
+   HomeConfig
 })
