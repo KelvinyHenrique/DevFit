68d94d4b80883d6e104028e9e9ac30c16e3ecffe
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-12 09:36:16 -0300

    WorkoutSelect Frase

--- src/screens/WorkoutSelect.js
+++ src/screens/WorkoutSelect.js
@@ -4,34 +4,50 @@ import { StackActions, NavigationActions } from 'react-navigation';
 import styled from 'styled-components/native';
 import { connect } from 'react-redux';
 import Workout from '../components/Workout';
-import {HeaderBackButton} from 'react-navigation-stack';
+import { HeaderBackButton } from 'react-navigation-stack';
 
 const Container = styled.SafeAreaView`
     flex:1;
+    margin:20px;
 `;
 
 const WorkoutList = styled.FlatList`
     flex:1;
-    padding:10px;
+`;
+
+const Title = styled.Text`
+    marginBottom:10px;
 `;
 
 const Page = (props) => {
-   
+
+    let lastWorkout = false;
+    if (props.lastWorkout) {
+        lastWorkout = props.myWorkouts.find(i => i.id == props.lastWorkout);
+    }
 
     const goWorkout = (workout) => {
-        props.navigation.navigate('WorkoutChecklist', {workout});
+        props.navigation.navigate('WorkoutChecklist', { workout });
     }
 
     return (
         <Container>
-            <WorkoutList 
-            data={props.myWorkouts}
-            renderItem={({item}) =>
-                <Workout
-                data={item}
-                goAction={()=>goWorkout(item)}
-                />
-            }/>
+            {lastWorkout &&
+                <>
+                    <Title>Seu último treino foi:</Title>
+                    <Workout data={lastWorkout} />
+                </>
+            }
+
+            <Title>Escolha seu treino de hoje:</Title>
+            <WorkoutList
+                data={props.myWorkouts}
+                renderItem={({ item }) =>
+                    <Workout
+                        data={item}
+                        goAction={() => goWorkout(item)}
+                    />
+                } />
 
         </Container>
     );
@@ -41,21 +57,22 @@ Page.navigationOptions = ({ navigation }) => {
 
     const handleBackAction = () => {
         navigation.dispatch(StackActions.reset({
-            index:0,
-            actions:[
-                NavigationActions.navigate({routeName:'AppTab'})
+            index: 0,
+            actions: [
+                NavigationActions.navigate({ routeName: 'AppTab' })
             ]
         }));
     }
 
     return {
         title: 'Escolha seu Treino',
-        headerLeft: () => <HeaderBackButton onPress={handleBackAction}/>
+        headerLeft: () => <HeaderBackButton onPress={handleBackAction} />
     }
 }
 
 const mapStateToProps = (state) => {
     return {
+        lastWorkout: state.userReducer.lastWorkout,
         myWorkouts: state.userReducer.myWorkouts
     }
 }
